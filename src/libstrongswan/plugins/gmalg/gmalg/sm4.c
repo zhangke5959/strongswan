40eefd800a1347e8c475a637a1cf9ffe4521114f
LinChen <lin.chen@e-quantum.com.cn>
2019-07-27 17:40:35 +0800

    Fix sm4 cbc decrypt failed when in and out buf address is same.

--- src/libstrongswan/plugins/gmalg/gmalg/sm4.c
+++ src/libstrongswan/plugins/gmalg/gmalg/sm4.c
@@ -257,17 +257,17 @@ void sm4_cbc_encrypt(sm4_ctx *ctx, u8 *key, u8 *iv, u8 *in, u8 len, u8 *out)
 #if DEBUG
 	printf(" function: %s ,  line= %d \n", __FUNCTION__, __LINE__);
 #endif
-	u8 temp[16];
 	int i;
 
 	sm4_set_key(ctx, key, 16);
-	memcpy(temp, iv, 16 );
 	while(len > 0)
 	{
 		for(i = 0; i < 16; i++)
-			out[i] = (u8)(in[i] ^ temp[i]);
+		{
+			out[i] = (u8)(in[i] ^ iv[i]);
+		}
 		sm4_one_round(ctx->sk_enc, out, out);
-		memcpy(temp, out, 16);
+		iv = out;
 		in  += 16;
 		out += 16;
 		len -= 16;
@@ -280,18 +280,34 @@ void sm4_cbc_decrypt(sm4_ctx *ctx, u8 *key, u8 *iv, u8 *in, u8 len, u8 *out)
 	printf(" function: %s ,  line= %d \n", __FUNCTION__, __LINE__);
 #endif
 	u8 temp[16];
+	u8 temp_iv[16];
 	int i;
 
 	sm4_set_key(ctx, key, 16);
-	memcpy(temp, iv, 16 );
+	
 	while(len > 0)
 	{
+		if(in == out)
+		{
+			memcpy(temp, in, 16); /* /if in and out are same buf, bakup in as next iv. */
+		}
 		sm4_one_round(ctx->sk_dec, in, out);
 		for(i = 0; i < 16; i++)
-			out[i] = (u8)(out[i] ^ temp[i] );
-		memcpy(temp, in, 16);
+		{
+			out[i] = (u8)(out[i] ^ iv[i]);
+		}
+		if(in == out)
+		{
+			memcpy(temp_iv, temp, 16);
+			iv = temp_iv;
+		}
+		else
+		{
+			iv = in;
+		}
 		in  += 16;
 		out += 16;
 		len -= 16;
 	}
+
 }
